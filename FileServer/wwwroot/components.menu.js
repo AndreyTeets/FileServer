0af3cb0e7221788d9c2c18157b68612c2fdc206d
AndreyTeets <AndreyTeets@gmail.com>
2025-11-06 20:22:38 +0300

    refactor: Move all parameterless lambdas in components to fields to avoid unnecessary changes in vElement state

--- FileServer/wwwroot/components.menu.js
+++ FileServer/wwwroot/components.menu.js
@@ -6,9 +6,13 @@ class MenuComponent extends ComponentBase {
     renderCore() {
         const div = VDom.createElement("div");
         div.append(new HeaderComponent().render({ text: "Menu:" }));
-        div.append(new ButtonComponent().render({ text: "DownloadPage", onclickFunc: () => this.props.selectPageFunc("download") }));
-        div.append(new ButtonComponent().render({ text: "UploadPage", onclickFunc: () => this.props.selectPageFunc("upload") }));
-        div.append(new ButtonComponent().render({ text: "AuthPage", onclickFunc: () => this.props.selectPageFunc("auth") }));
+        div.append(new ButtonComponent().render({ text: "DownloadPage", onclickFunc: this.#onDownloadPageClick }));
+        div.append(new ButtonComponent().render({ text: "UploadPage", onclickFunc: this.#onUploadPageClick }));
+        div.append(new ButtonComponent().render({ text: "AuthPage", onclickFunc: this.#onAuthPageClick }));
         return div;
     }
+
+    #onDownloadPageClick = () => this.props.selectPageFunc("download");
+    #onUploadPageClick = () => this.props.selectPageFunc("upload");
+    #onAuthPageClick = () => this.props.selectPageFunc("auth");
 }
