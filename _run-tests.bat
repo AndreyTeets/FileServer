4ce26fdddaae73732e7d4a79427fe4fe0d5eb23a
AndreyTeets <AndreyTeets@gmail.com>
2025-11-09 07:22:25 +0300

    other: Add bat-script to run tests and commands for trivy/codeql scanning locally

---
+++ _run-tests.bat
@@ -0,0 +1,30 @@
+@echo off
+pushd "%~dp0"
+
+echo -^> Building solution...
+dotnet build -c Release -v q
+if %errorlevel% neq 0 goto :Error
+
+echo -^> Format verifying solution...
+dotnet format --verify-no-changes --no-restore -v q
+if %errorlevel% neq 0 goto :Error
+
+echo -^> Running tests...
+dotnet test FileServer.Tests -c Release --no-build -v q
+if %errorlevel% neq 0 goto :Error
+
+echo -^> Installing Playwright...
+pwsh FileServer.E2ETests/bin/Release/net8.0/playwright.ps1 install --only-shell chromium
+if %errorlevel% neq 0 goto :Error
+
+echo -^> Running E2E tests...
+dotnet test FileServer.E2ETests -c Release --no-build -v q
+if %errorlevel% neq 0 goto :Error
+goto :End
+
+:Error
+:End
+echo;
+echo Press any key to close...
+pause>nul
+popd
