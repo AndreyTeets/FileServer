806f59cc1e54ff309b64783a910db10fc8e6e8df
AndreyTeets <AndreyTeets@gmail.com>
2026-01-08 22:50:54 +0300

    test: Add FileServiceTests/TokenServiceTests/DebouncerTests/InMemoryXmlRepositoryTests

---
+++ tests/FileServer.Tests/FileServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using FileServer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using FileInfo = FileServer.Models.FileInfo;
+
+namespace FileServer.Tests;
+
+internal sealed class FileServiceTests : TestsBase
+{
+#pragma warning disable CS8618 // Non-nullable variable must contain a non-null value when exiting constructor. Consider declaring it as nullable.
+    private FileService _fileService;
+#pragma warning restore CS8618 // Remove when `dotnet format` is fixed (see https://github.com/dotnet/sdk/issues/44867)
+
+    [SetUp]
+    public void SetUp()
+    {
+        _fileService = TestServer.Services.GetRequiredService<FileService>();
+    }
+
+    [Test]
+    public async Task GetDirectoryFilesListRecursive_FindsAllFiles()
+    {
+        List<FileInfo> x = _fileService.GetDirectoryFilesListRecursive(Path.GetFullPath("fs_data"));
+        Assert.That(x.Select(x => x.Path), Is.EquivalentTo(
+        [   // All files in upload folder are deleted in the tests base setup
+            "download/file1.txt",
+            "download_anon/anonfile1.txt",
+        ]));
+    }
+
+    [TestCase("", "upl..oad")]
+    [TestCase("09AZaz !#$%&'()+,-.;=@[]^_`{}~", "upl.09AZaz !#$%&'()+,-.;=@[]^_`{}~.oad")]
+    [TestCase("../X", "upl..._X.oad")]
+    [TestCase("1 π 2 π€Й 3 🥰 4 \u222B 5 \ud83e\udd70 0", "upl.1 _ 2 ___ 3 __ 4 _ 5 __ 0.oad")]
+    public async Task SaveFileIfNotExists_SavesFileWithSanitizedName(string fileName, string expectedFileName)
+    {
+        string fileContentText = $"{fileName}\r?\n{expectedFileName}\n";
+        using CancellationTokenSource cts = new();
+        await using Stream fileContent = CreateFileContent(fileContentText);
+
+        (string targetFileName, bool saved) = await _fileService.SaveFileIfNotExists(fileName, fileContent, cts.Token);
+        Assert.That(saved, Is.True);
+        Assert.That(targetFileName, Is.EqualTo(expectedFileName));
+
+        string targetFilePath = $"fs_data/upload/{targetFileName}";
+        Assert.That(File.Exists(targetFilePath), Is.True);
+        Assert.That(await File.ReadAllTextAsync(targetFilePath), Is.EqualTo(fileContentText));
+    }
+
+    [Test]
+    public async Task SaveFileIfNotExists_SkipsSavingFileWhenExists()
+    {
+        const string fileName = "does_not_matter";
+        const string fileContentText = "does_not_matter";
+        using CancellationTokenSource cts = new();
+        await using Stream fileContent = CreateFileContent(fileContentText);
+
+        (string _, bool saved1) = await _fileService.SaveFileIfNotExists(fileName, fileContent, cts.Token);
+        Assert.That(saved1, Is.True);
+        (string _, bool saved2) = await _fileService.SaveFileIfNotExists(fileName, fileContent, cts.Token);
+        Assert.That(saved2, Is.False);
+    }
+
+    private static Stream CreateFileContent(string text) =>
+        new MemoryStream(Encoding.UTF8.GetBytes(text));
+}
---
+++ tests/FileServer.Tests/TokenServiceTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Net;
+using System.Text;
+using FileServer.Models;
+using FileServer.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileServer.Tests;
+
+internal sealed class TokenServiceTests : TestsBase
+{
+#pragma warning disable CS8618 // Non-nullable variable must contain a non-null value when exiting constructor. Consider declaring it as nullable.
+    private TokenService _tokenService;
+#pragma warning restore CS8618 // Remove when `dotnet format` is fixed (see https://github.com/dotnet/sdk/issues/44867)
+
+    [SetUp]
+    public void SetUp()
+    {
+        _tokenService = TestServer.Services.GetRequiredService<TokenService>();
+    }
+
+    [Test]
+    public async Task Decode_EncodedToken_IsEquivalentToOriginal()
+    {
+        Token token = _tokenService.CreateToken(CreateTestClaim());
+        string encodedTokenString = _tokenService.EncodeToken(token);
+        Token? decodedToken = _tokenService.TryDecodeToken(encodedTokenString);
+
+        Assert.That(decodedToken, Is.Not.Null);
+        Assert.That(decodedToken.Claim, Is.Not.Null);
+        Assert.That(decodedToken.Claim.User, Is.EqualTo(token.Claim!.User));
+        Assert.That(decodedToken.Claim.Type, Is.EqualTo(token.Claim.Type));
+        Assert.That(decodedToken.Claim.Expires, Is.EqualTo(token.Claim.Expires));
+        Assert.That(decodedToken.Signature, Is.EqualTo(token.Signature));
+    }
+
+    [Test]
+    public async Task Decode_ReturnsCorrectToken_ForProperInput()
+    {
+        const string tokenJson = /*lang=json,strict*/
+            @"{ ""claim"": { ""user"": ""111"", ""type"": ""111"", ""expires"": ""2026-01-01T22:33:44.5556667Z"" }, ""signature"": ""111"" }";
+        Token? token = _tokenService.TryDecodeToken(EncodeTokenJson(tokenJson));
+
+        Assert.That(token, Is.Not.Null);
+        Assert.That(token.Claim, Is.Not.Null);
+        Assert.That(token.Claim.User, Is.EqualTo("111"));
+        Assert.That(token.Claim.Type, Is.EqualTo("111"));
+        Assert.That(token.Claim.Expires?.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ"), Is.EqualTo("2026-01-01T22:33:44.5556667Z"));
+        Assert.That(token.Signature, Is.EqualTo("111"));
+    }
+
+    [TestCase(/*lang=json,strict*/ "{}")]
+    [TestCase(/*lang=json,strict*/ @"{ ""somefield"": ""111"" }")]
+    [TestCase(/*lang=json,strict*/ @"{ ""signature"": ""111"" }")]
+    [TestCase(/*lang=json,strict*/ @"{ ""claim"": { }, ""signature"": ""111"" }")]
+    [TestCase(/*lang=json,strict*/ @"{ ""claim"": { ""user"": ""111"" }, ""signature"": ""111"" }")]
+    public async Task Decode_ReturnsToken_ForMalformedJsonInput(string tokenJson)
+    {
+        Token? token = _tokenService.TryDecodeToken(EncodeTokenJson(tokenJson));
+        Assert.That(token, Is.Not.Null);
+    }
+
+    [TestCase(/*lang=json,strict*/ "")]
+    public async Task Decode_ReturnsNull_ForMalformedInput(string tokenJson)
+    {
+        Token? token = _tokenService.TryDecodeToken(EncodeTokenJson(tokenJson));
+        Assert.That(token, Is.Null);
+    }
+
+    [Test]
+    public async Task IsValid_ReturnsFalse_WhenBadSignature()
+    {
+        Token token = new() { Claim = CreateTestClaim(), Signature = "some_bad_signature" };
+        bool isValid = _tokenService.TokenIsValid(token);
+        Assert.That(isValid, Is.False);
+    }
+
+    [Test]
+    public async Task IsValid_ReturnsFalse_WhenExpired()
+    {
+        Token token = _tokenService.CreateToken(CreateTestClaim(expired: true));
+        bool isValid = _tokenService.TokenIsValid(token);
+        Assert.That(isValid, Is.False);
+    }
+
+    [Test]
+    public async Task IsValid_ReturnsTrue_WhenNormallyCreated()
+    {
+        Token token = _tokenService.CreateToken(CreateTestClaim());
+        bool isValid = _tokenService.TokenIsValid(token);
+        Assert.That(isValid, Is.True);
+    }
+
+    private static string EncodeTokenJson(string tokenJson)
+    {
+        string tokenBase64String = Convert.ToBase64String(Encoding.UTF8.GetBytes(tokenJson));
+        return WebUtility.UrlEncode(tokenBase64String);
+    }
+
+    private static Claim CreateTestClaim(bool expired = false) => new()
+    {
+        User = "some_user_name",
+        Type = "some_token_type",
+        Expires = DateTime.UtcNow.AddSeconds(expired ? -60 : 60),
+    };
+}
