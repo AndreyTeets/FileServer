1c4ea3ca38f24b4fc84864d0b69f9bf4b47c3448
AndreyTeets <AndreyTeets@gmail.com>
2026-01-11 14:34:45 +0300

    refactor: Split AuthRoutesHandler/FilesRoutesHandler into more granular handlers

---
+++ src/FileServer/Routes/Auth/Logout/AuthLogoutHandler.cs
@@ -0,0 +1,25 @@
+﻿using FileServer.Configuration;
+
+namespace FileServer.Routes.Auth.Logout;
+
+internal sealed class AuthLogoutHandler(
+    IHttpContextAccessor httpContextAccessor)
+    : IRouteHandler<AuthLogoutParams>
+{
+    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+    private HttpRequest Request => _httpContextAccessor.HttpContext!.Request;
+    private HttpResponse Response => _httpContextAccessor.HttpContext!.Response;
+
+    public async Task<IResult> Execute(AuthLogoutParams routeParams)
+    {
+        DeleteAuthTokenCookie();
+        return Results.Ok();
+    }
+
+    private void DeleteAuthTokenCookie()
+    {
+        CookieOptions cookieOptions = StaticSettings.GetAuthTokenCookieOptions(Request.Host.Host);
+        Response.Cookies.Delete(Constants.AuthTokenCookieName, cookieOptions);
+    }
+}
