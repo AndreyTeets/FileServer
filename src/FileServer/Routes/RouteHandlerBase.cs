64e4dd881020d804bb0cd5c9836ec55224825f0a
AndreyTeets <AndreyTeets@gmail.com>
2026-01-21 22:29:33 +0300

    refactor: Move IsAuthenticated method from FilesListHandler to RouteHandlerBase

--- src/FileServer/Routes/RouteHandlerBase.cs
+++ src/FileServer/Routes/RouteHandlerBase.cs
@@ -1,12 +1,23 @@
-﻿namespace FileServer.Routes;
+﻿using System.Security.Claims;
+
+namespace FileServer.Routes;
 
 internal abstract class RouteHandlerBase(
     IHttpContextAccessor httpContextAccessor)
 {
     private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
 
-    private protected HttpContext Context => _httpContextAccessor.HttpContext!;
-    private protected HttpRequest Request => Context.Request;
-    private protected HttpResponse Response => Context.Response;
-    private protected CancellationToken Ct => Context.RequestAborted;
+    protected HttpContext Context => _httpContextAccessor.HttpContext!;
+    protected HttpRequest Request => Context.Request;
+    protected HttpResponse Response => Context.Response;
+    protected CancellationToken Ct => Context.RequestAborted;
+
+    protected bool IsAuthenticated()
+    {
+        ClaimsPrincipal user = Context.User;
+        return user.Identities.Any(x =>
+            x.IsAuthenticated &&
+            x.AuthenticationType == Constants.DoubleTokenAuthenticationSchemeAuthenticationType &&
+            x.Name == Constants.MainUserName);
+    }
 }
