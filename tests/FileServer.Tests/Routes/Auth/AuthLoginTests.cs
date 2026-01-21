43774f9fc47c0d94a1e594266b64d4ca2947656d
AndreyTeets <AndreyTeets@gmail.com>
2026-01-21 22:14:11 +0300

    test: Reorder AuthLoginTests more similarly to test explorer

--- tests/FileServer.Tests/Routes/Auth/AuthLoginTests.cs
+++ tests/FileServer.Tests/Routes/Auth/AuthLoginTests.cs
@@ -9,6 +9,26 @@ namespace FileServer.Tests.Routes.Auth;
 
 internal sealed class AuthLoginTests : ServerTestsBase
 {
+    [TestCase(/*lang=json,strict*/ null, HttpStatusCode.BadRequest)]
+    [TestCase(/*lang=json,strict*/ "", HttpStatusCode.BadRequest)]
+    [TestCase(/*lang=json,strict*/ "{}", HttpStatusCode.BadRequest)]
+    [TestCase(/*lang=json,strict*/ @"{ ""SomeField"": ""111"" }", HttpStatusCode.BadRequest)]
+    [TestCase(/*lang=json,strict*/ @"{ ""Password"": 111 }", HttpStatusCode.BadRequest)]
+    [TestCase(/*lang=json,strict*/ @"{ ""Password"": { ""SomeField"": ""111"" } }", HttpStatusCode.BadRequest)]
+    [TestCase(/*lang=json,strict*/ @"{ ""Password"": null }", HttpStatusCode.Unauthorized)]
+    [TestCase(/*lang=json,strict*/ @"{ ""Password"": ""111"" }", HttpStatusCode.Unauthorized)]
+    public async Task Login_Fails_OnWrongRequest(
+        string? requestBodyJsonStr, HttpStatusCode expectedResponseStatusCode)
+    {
+        using HttpResponseMessage response = await FsTestClient.Post("/api/auth/login",
+            content: CreateJsonContent(requestBodyJsonStr));
+        Assert.That(response.StatusCode, Is.EqualTo(expectedResponseStatusCode));
+
+        static StringContent? CreateJsonContent(string? jsonStr) => jsonStr is not null
+            ? new(jsonStr, MediaTypeHeaderValue.Parse(MediaTypeNames.Application.Json))
+            : null;
+    }
+
     [Test]
     public async Task Login_ProducesCorrectResponse_WhenSuccessful()
     {
@@ -38,25 +58,5 @@ internal sealed class AuthLoginTests : ServerTestsBase
             Assert.That(token.Claim.Expires, Is.EqualTo(loginResponse.LoginInfo.TokensExpire));
             Assert.That(tokenService.TokenIsValid(token), Is.True);
         }
-    }
-
-    [TestCase(/*lang=json,strict*/ null, HttpStatusCode.BadRequest)]
-    [TestCase(/*lang=json,strict*/ "", HttpStatusCode.BadRequest)]
-    [TestCase(/*lang=json,strict*/ "{}", HttpStatusCode.BadRequest)]
-    [TestCase(/*lang=json,strict*/ @"{ ""SomeField"": ""111"" }", HttpStatusCode.BadRequest)]
-    [TestCase(/*lang=json,strict*/ @"{ ""Password"": 111 }", HttpStatusCode.BadRequest)]
-    [TestCase(/*lang=json,strict*/ @"{ ""Password"": { ""SomeField"": ""111"" } }", HttpStatusCode.BadRequest)]
-    [TestCase(/*lang=json,strict*/ @"{ ""Password"": null }", HttpStatusCode.Unauthorized)]
-    [TestCase(/*lang=json,strict*/ @"{ ""Password"": ""111"" }", HttpStatusCode.Unauthorized)]
-    public async Task Login_Fails_OnWrongRequest(
-        string? requestBodyJsonStr, HttpStatusCode expectedResponseStatusCode)
-    {
-        using HttpResponseMessage response = await FsTestClient.Post("/api/auth/login",
-            content: CreateJsonContent(requestBodyJsonStr));
-        Assert.That(response.StatusCode, Is.EqualTo(expectedResponseStatusCode));
-
-        static StringContent? CreateJsonContent(string? jsonStr) => jsonStr is not null
-            ? new(jsonStr, MediaTypeHeaderValue.Parse(MediaTypeNames.Application.Json))
-            : null;
     }
 }
