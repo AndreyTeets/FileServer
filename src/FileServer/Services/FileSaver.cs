AndreyTeets/FileServer

commit 2c33986bd770363907b7bcef31fdb47a23caf25f

fix: Rewrite file existence check in FileSaver to os-level file-locking to avoid race condition

src/FileServer/Services/FileSaver.cs
@@ -12,16 +12,18 @@ internal sealed class FileSaver(
     public async Task<(string targetFileName, bool saved)> SaveFileIfNotExists(
         string originalFileName, Stream fileContent, CancellationToken ct)
     {
-        string trustedFileName = SanitizeFileName(originalFileName);
-        string saveToPath = Path.Combine(_options.CurrentValue.UploadDir, trustedFileName);
-
-        if (!File.Exists(saveToPath))
+        string sanitizedFileName = SanitizeFileName(originalFileName);
+        string saveToPath = Path.Combine(_options.CurrentValue.UploadDir, sanitizedFileName);
+        try
         {
-            await using FileStream targetStream = File.Create(saveToPath);
+            await using FileStream targetStream = new(saveToPath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
             await fileContent.CopyToAsync(targetStream, ct);
-            return (trustedFileName, true);
+            return (sanitizedFileName, true);
         }
-        return (trustedFileName, false);
+        catch (IOException) when (File.Exists(saveToPath))
+        {
+            return (sanitizedFileName, false);
+        }
     }
 
     private static string SanitizeFileName(string fileName)

commit 359e78f8a4e236dc7e29463208e5343fb81ed357

fix: Don't return 409 (Conflict) in file upload on file save error after successfully creating it
This incorrect behavior was introduced in the 2c33986bd770 commit and hasn't been included in any release since then.

src/FileServer/Services/FileSaver.cs
@@ -14,13 +14,15 @@ internal sealed class FileSaver(
     {
         string sanitizedFileName = SanitizeFileName(originalFileName);
         string saveToPath = Path.Combine(_options.CurrentValue.UploadDir, sanitizedFileName);
+        bool createdTargetFile = false;
         try
         {
             await using FileStream targetStream = new(saveToPath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+            createdTargetFile = true;
             await fileContent.CopyToAsync(targetStream, ct);
             return (sanitizedFileName, true);
         }
-        catch (IOException) when (File.Exists(saveToPath))
+        catch (IOException) when (!createdTargetFile && File.Exists(saveToPath))
         {
             return (sanitizedFileName, false);
         }